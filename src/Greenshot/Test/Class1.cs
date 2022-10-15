f9b3bff4efb3382db55fa73194c09cea32ef70c5
Nathan Brown <nathanofbrown@gmail.com>
2022-10-14 21:34:35 -0700

    Test skeleton

---
+++ src/Greenshot/Test/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Greenshot.Editor.Forms;
+using NUnit.Framework;
+
+namespace Greenshot.Test
+{
+    class Class1
+    {
+        [Test]
+        public void Test1()
+        {
+            var x = new KeyEventArgs(Keys.Z);
+            //ImageEditorForm.ImageEditorFormKeyDown(null, x);
+            //SendKeys.Send("z");
+        }
+    }
+}
