9105f6794a2e6aaa19f423912db1557e52fd00e0
Musa-Husseini <74230559+Musa-Husseini@users.noreply.github.com>
2022-10-31 13:57:24 -0700

    Removed test files

---
+++ src/Feature-1380 Tests/UnitTest1.cs
@@ -0,0 +1,25 @@
+namespace Feature_1380_Tests
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using System.Windows;
+    using NUnit.Framework;
+    using Greenshot;
+
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+
+        }
+    }
+}
